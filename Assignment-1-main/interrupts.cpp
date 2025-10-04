380d1145506760de81ac19f73fc14ec9ac62ffc7
aliooo36 <izhan1642@gmail.com>
2025-10-04 16:52:33 -0400

    updated interrupts.cpp, made syscall function

--- Assignment-1-main/interrupts.cpp
+++ Assignment-1-main/interrupts.cpp
@@ -1,7 +1,7 @@
 /**
  *
  * @file interrupts.cpp
- * @author Sasisekhar Govind
+ * @author Muhammad Ali & Gregory Horvat
  *
  */
 
@@ -20,9 +20,9 @@ int main(int argc, char** argv) {
 
     /******************ADD YOUR VARIABLES HERE*************************/
 
-    int current_time = 0;
-    const int context_SR_time = 10;
-    std::vector<int> deviceTimers = delays;
+    int currentTime = 0;
+    const int contextSRTime = 10;
+    const int ISRActivityTime = 40;
 
     /******************************************************************/
 
@@ -33,28 +33,32 @@ int main(int argc, char** argv) {
         /******************ADD YOUR SIMULATION CODE HERE*************************/
 
         if (activity == "CPU") {
-            execution += std::to_string(current_time) + ", " + std::to_string(duration_intr) + ", CPU\n";
-            current_time += duration_intr;
+            execution += std::to_string(currentTime) + ", " + std::to_string(duration_intr) + ", CPU\n";
+            currentTime += duration_intr;
         }
+
         else if (activity == "SYSCALL") {
             int device_num = duration_intr;
-            execution += std::to_string(current_time) + ", " + std::to_string(1) + ", SYSCALL " + std::to_string(device_num) + "\n";
-            current_time += 1;
+
+            // checking to see if ISR is in vector table
+            if (duration_intr >= vectors.size()) { 
+                execution += std::to_string(currentTime) + ", Error: Invalid ISR. Does not exist in vector table.";
+                currentTime += 1;
+                continue;
+            }
+        
+            auto [intrpExec, updateTime] = intr_boilerplate(currentTime, device_num, contextSRTime, vectors); // helper function execution and then storing in variables
+            execution += intrpExec;
+            currentTime += updateTime;
+
+            execution += std::to_string(currentTime) + " ," + std::to_string(ISRActivityTime) + " , Call device driver";
+            currentTime += ISRActivityTime;
+
+            execution += std::to_string(currentTime) + " , 1" + ", IRET";
+
         }
         else if (activity == "END_IO") {
-            int device_num = duration_intr;  
-            
-            auto [intr_execution, updated_time] = intr_boilerplate(current_time, device_num, context_SR_time, vectors);
-            
-            execution += intr_execution;
-            current_time = updated_time;
-            
-            int device_io_time = delays[device_num];
-            execution += std::to_string(current_time) + ", " + std::to_string(device_io_time) + ", call device driver\n";
-            current_time += device_io_time;
-        
-            execution += std::to_string(current_time) + ", " + std::to_string(1) + ", IRET\n";
-            current_time += 1;
+            continue;
         }
 
         /************************************************************************/
